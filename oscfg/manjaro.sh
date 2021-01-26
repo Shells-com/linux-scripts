4a00c817e9fd6d3db2060455f2c09cfd9d70ee72
Philip Müller <philm@manjaro.org>
2021-01-26 10:55:42 +0100

    manjaro.sh: modify script
    - XFCE: adjust packages into more lines
    - Plasma: add initial package list of Manjaro KDE edition

--- oscfg/manjaro.sh
+++ oscfg/manjaro.sh
@@ -26,7 +26,11 @@ manjaro_cfg() {
 	# ensure desktop installation & guest tools
 	case "$1" in
 		manjaro-desktop)
-			run pacman -S --noconfirm xfce4 ttf-dejavu lightdm-gtk-greeter-settings accountsservice xfce4-goodies xfce4-pulseaudio-plugin pulseaudio pavucontrol mugshot engrampa catfish firefox screenfetch thunderbird network-manager-applet pamac-gtk xf86-input-libinput xf86-video-qxl-debian xorg-server xorg-mkfontscale xorg-xkill noto-fonts noto-fonts-cjk inxi nano manjaro-xfce-settings manjaro-hello manjaro-application-utility manjaro-settings-manager-notifier manjaro-documentation-en manjaro-browser-settings manjaro-release manjaro-firmware manjaro-system phodav spice-vdagent
+			run pacman -S --noconfirm xfce4 ttf-dejavu lightdm-gtk-greeter-settings accountsservice xfce4-goodies xfce4-pulseaudio-plugin mugshot engrampa catfish screenfetch network-manager-applet noto-fonts noto-fonts-cjk
+			run pacman -S --noconfirm manjaro-xfce-settings manjaro-release manjaro-firmware manjaro-system manjaro-hello manjaro-application-utility manjaro-settings-manager-notifier manjaro-documentation-en manjaro-browser-settings nano inxi
+			run pacman -S --noconfirm firefox thunderbird
+			run pacman -S --noconfirm pulseaudio pavucontrol 
+			run pacman -S --noconfirm xf86-input-libinput xf86-video-qxl-debian xorg-server xorg-mkfontscale xorg-xkill phodav spice-vdagent
 			run pacman -S --noconfirm pamac-gtk pamac-snap-plugin pamac-flatpak-plugin
 			run systemctl enable lightdm
 			run systemctl enable apparmor snapd snapd.apparmor
@@ -48,6 +52,141 @@ position = 50%,center 57%,center
 clock-format =
 panel-position = bottom
 indicators = ~host;~spacer;~clock;~spacer;~language;~session;~a11y;~power
+EOF
+			;;
+		manjaro-kde-desktop)
+			run pacman -S --noconfirm plasma-meta ark dolphin dolphin-plugins kate kcalc kfind okular kget libktorrent kdenetwork-filesharing kio-extras konsole konversation ksystemlog kwalletmanager gwenview spectacle kdegraphics-thumbnailers ffmpegthumbs ruby kimageformats qt5-imageformats systemd-kcm yakuake vlc oxygen oxygen-icons kaccounts-providers
+			run pacman -S --noconfirm manjaro-kde-settings manjaro-release manjaro-firmware manjaro-system manjaro-hello manjaro-application-utility manjaro-settings-manager-notifier manjaro-documentation-en manjaro-browser-settings manjaro-settings-manager-kcm manjaro-settings-manager-knotifier sddm-breath2-theme nano inxi illyria-wallpaper wallpapers-juhraya wallpapers-2018 manjaro-wallpapers-18.0
+			run pacman -S --noconfirm firefox thunderbird
+			run pacman -S --noconfirm pulseaudio pavucontrol 
+			run pacman -S --noconfirm xf86-input-libinput xf86-video-qxl-debian xorg-server xorg-mkfontscale xorg-xkill phodav spice-vdagent
+			run pacman -S --noconfirm pamac-gtk pamac-snap-plugin pamac-flatpak-plugin xdg-desktop-portal xdg-desktop-portal-kde
+			run systemctl enable lightdm
+			run systemctl enable apparmor snapd snapd.apparmor
+
+			cat >"$WORK/etc/sddm.conf" <<EOF
+[Autologin]
+# Whether sddm should automatically log back into sessions when they exit
+Relogin=false
+
+# Name of session file for autologin session (if empty try last logged in)
+Session=plasma.desktop
+
+# Username for autologin session
+User=
+
+[General]
+# Halt command
+HaltCommand=/usr/bin/systemctl poweroff
+
+# Input method module
+InputMethod=
+
+# Initial NumLock state. Can be on, off or none.
+# If property is set to none, numlock won't be changed
+# NOTE: Currently ignored if autologin is enabled.
+Numlock=none
+
+# Reboot command
+RebootCommand=/usr/bin/systemctl reboot
+
+[Theme]
+# Current theme name
+Current=breath2
+
+# Cursor theme used in the greeter
+CursorTheme=breeze_cursors
+
+# Number of users to use as threshold
+# above which avatars are disabled
+# unless explicitly enabled with EnableAvatars
+DisableAvatarsThreshold=7
+
+# Enable display of custom user avatars
+EnableAvatars=true
+
+# Global directory for user avatars
+# The files should be named <username>.face.icon
+FacesDir=/usr/share/sddm/faces
+
+# Theme directory path
+ThemeDir=/usr/share/sddm/themes
+
+[Users]
+# Default $PATH for logged in users
+DefaultPath=/usr/local/sbin:/usr/local/bin:/usr/bin
+
+# Comma-separated list of shells.
+# Users with these shells as their default won't be listed
+HideShells=
+
+# Comma-separated list of users that should not be listed
+HideUsers=
+
+# Maximum user id for displayed users
+MaximumUid=60000
+
+# Minimum user id for displayed users
+MinimumUid=1000
+
+# Remember the session of the last successfully logged in user
+RememberLastSession=true
+
+# Remember the last successfully logged in user
+RememberLastUser=true
+
+# When logging in as the same user twice, restore the original session, rather than create a new one
+ReuseSession=false
+
+[Wayland]
+# Enable Qt's automatic high-DPI scaling
+EnableHiDPI=false
+
+# Path to a script to execute when starting the desktop session
+SessionCommand=/usr/share/sddm/scripts/wayland-session
+
+# Directory containing available Wayland sessions
+SessionDir=/usr/share/wayland-sessions
+
+# Path to the user session log file
+SessionLogFile=.local/share/sddm/wayland-session.log
+
+[X11]
+# Path to a script to execute when starting the display server
+DisplayCommand=/usr/share/sddm/scripts/Xsetup
+
+# Path to a script to execute when stopping the display server
+DisplayStopCommand=/usr/share/sddm/scripts/Xstop
+
+# Enable Qt's automatic high-DPI scaling
+EnableHiDPI=false
+
+# The lowest virtual terminal number that will be used.
+MinimumVT=1
+
+# Arguments passed to the X server invocation
+ServerArguments=-nolisten tcp
+
+# Path to X server binary
+ServerPath=/usr/bin/X
+
+# Path to a script to execute when starting the desktop session
+SessionCommand=/usr/share/sddm/scripts/Xsession
+
+# Directory containing available X sessions
+SessionDir=/usr/share/xsessions
+
+# Path to the user session log file
+SessionLogFile=.local/share/sddm/xorg-session.log
+
+# Path to the Xauthority file
+UserAuthFile=.Xauthority
+
+# Path to xauth binary
+XauthPath=/usr/bin/xauth
+
+# Path to Xephyr binary
+XephyrPath=/usr/bin/Xephyr
 EOF
 			;;
 		manjaro-openssh)
