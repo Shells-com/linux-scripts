5da7c6a795bef924bfdeae941a0c66c0c7a495d6
Mark Karpeles <magicaltux@gmail.com>
2021-02-21 21:59:28 +0900

    always overwrite test image

--- test-linux.sh
+++ test-linux.sh
@@ -6,22 +6,20 @@
 
 # run a given .qcow2 image in qemu using a configuration similar to shells
 
-if [ ! -f res/test.qcow2 ]; then
-	# need to take image in first arg. Let's create a 160GB image out of it.
-	if [ x"$1" = x ]; then
-		echo "Usage: $0 file.qcow2"
-		echo "Will start a qemu instance using this file. The qcow2 file will not be modified"
-		exit 1
-	fi
-	if [ ! -f "$1" ]; then
-		echo "Usage: $0 file.qcow2"
-		echo "File $1 was not found"
-		exit 1
-	fi
-
-	# create [--object objectdef] [-q] [-f fmt] [-b backing_file] [-F backing_fmt] [-u] [-o options] filename [size]
-	"$QEMUIMG" create -f qcow2 -b "$(realpath $1)" -F qcow2 "res/test.qcow2" 160G
+# need to take image in first arg. Let's create a 160GB image out of it.
+if [ x"$1" = x ]; then
+	echo "Usage: $0 file.qcow2"
+	echo "Will start a qemu instance using this file. The qcow2 file will not be modified"
+	exit 1
 fi
+if [ ! -f "$1" ]; then
+	echo "Usage: $0 file.qcow2"
+	echo "File $1 was not found"
+	exit 1
+fi
+
+# always create (overwrite) the image
+"$QEMUIMG" create -f qcow2 -b "$(realpath $1)" -F qcow2 "res/test.qcow2" 160G
 
 # launch qemu
 qemukernel res/test.qcow2
