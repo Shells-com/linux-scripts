666e9e0dc8ce9ae175b344ab55ad8b1d8b7099d2
zgrimshell <zlatan@riseup.net>
2021-02-22 21:08:53 +0100

    disable logout

--- oscfg/ubuntu.sh
+++ oscfg/ubuntu.sh
@@ -175,6 +175,7 @@ EOF
 gsettings set org.gnome.desktop.screensaver lock-enabled false
 gsettings set org.gnome.desktop.screensaver idle-activation-enabled false
 gsettings set org.gnome.desktop.lockdown disable-lock-screen true
+gsettings set org.gnome.desktop.lockdown disable-log-out true
 gsettings set org.gnome.desktop.session idle-delay 0
 gsettings set org.gnome.settings-daemon.plugins.power active false
 
