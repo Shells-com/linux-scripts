d03523a2cd40055934bfa258e12afc3a96c72e23
zgrimshell <zlatan@riseup.net>
2021-03-19 17:51:08 +0100

    pam-autologin in lightdm might not be needed

--- scripts/firstrun.sh
+++ scripts/firstrun.sh
@@ -97,7 +97,7 @@ if [ x"$SHELLS_USERNAME" != x ]; then
 		gpasswd -a "${SHELLS_USERNAME}" autologin
 		sed -i -e "s/^.*autologin-user=.*/autologin-user=${SHELLS_USERNAME}/" /etc/lightdm/lightdm.conf
 		sed -i -e "s/^.*autologin-user-timeout=.*/autologin-user-timeout=0/" /etc/lightdm/lightdm.conf
-		sed -i -e "s/^.*pam-autologin-service=.*/pam-autologin-service=lightdm-autologin/" /etc/lightdm/lightdm.conf
+#		sed -i -e "s/^.*pam-autologin-service=.*/pam-autologin-service=lightdm-autologin/" /etc/lightdm/lightdm.conf
 		echo "auth        sufficient  pam_succeed_if.so user ingroup nopasswdlogin" >> /etc/pam.d/lightdm
 		groupadd -r nopasswdlogin
 		gpasswd -a "${SHELLS_USERNAME}" nopasswdlogin
