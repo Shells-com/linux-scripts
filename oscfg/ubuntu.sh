46bdb301c5c8ccf5a096133ac1bdaf7a47111735
zgrimshell <zlatan@riseup.net>
2021-04-05 20:37:35 +0200

    more xubuntu power settings

--- oscfg/ubuntu.sh
+++ oscfg/ubuntu.sh
@@ -153,6 +153,36 @@ EOF
 </channel>
 EOF
 
+			cat >"$WORK/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-power-manager.xml" <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+
+<channel name="xfce4-power-manager" version="1.0">
+  <property name="xfce4-power-manager" type="empty">
+    <property name="power-button-action" type="empty"/>
+    <property name="lock-screen-suspend-hibernate" type="bool" value="false"/>
+    <property name="logind-handle-lid-switch" type="empty"/>
+    <property name="blank-on-ac" type="int" value="0"/>
+    <property name="blank-on-battery" type="empty"/>
+    <property name="dpms-enabled" type="bool" value="false"/>
+    <property name="dpms-on-ac-sleep" type="uint" value="0"/>
+    <property name="dpms-on-ac-off" type="uint" value="0"/>
+    <property name="dpms-on-battery-sleep" type="uint" value="0"/>
+    <property name="dpms-on-battery-off" type="uint" value="0"/>
+    <property name="hibernate-button-action" type="uint" value="0"/>
+    <property name="sleep-button-action" type="uint" value="0"/>
+    <property name="lid-action-on-ac" type="uint" value="0"/>
+    <property name="critical-power-action" type="uint" value="0"/>
+    <property name="lid-action-on-battery" type="uint" value="0"/>
+    <property name="inactivity-on-ac" type="uint" value="0"/>
+    <property name="brightness-switch-restore-on-exit" type="int" value="0"/>
+    <property name="brightness-switch" type="int" value="0"/>
+    <property name="brightness-level-on-ac" type="uint" value="100"/>
+    <property name="presentation-mode" type="bool" value="false"/>
+  </property>
+</channel>
+EOF
+
+
 	esac
 
 	# ensure guest tools
