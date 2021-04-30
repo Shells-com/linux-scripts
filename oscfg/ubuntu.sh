859b7b7dfcc3f9ba34252f9c2d68de0e6c7850e6
zgrimshell <zlatan@riseup.net>
2021-04-30 10:58:42 +0200

    add bash-completion, nano to ubuntu desktop

--- oscfg/ubuntu.sh
+++ oscfg/ubuntu.sh
@@ -209,7 +209,7 @@ EOF
 	# if gnome
 	case "$1" in
 		ubuntu-*-ubuntu-desktop)
-			DEBIAN_FRONTEND=noninteractive run apt-get install -y gnome-software guake ubuntu-release-upgrader-core ubuntu-desktop
+			DEBIAN_FRONTEND=noninteractive run apt-get install -y gnome-software guake ubuntu-release-upgrader-core ubuntu-desktop bash-completion nano
 			run appstreamcli refresh --force && run apt update
 			;;
 	esac
